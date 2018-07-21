543bcc84cef92f54f3da717ffa9e14d295f674f0
Jochen Peters <dev@emirp.net>
2018-07-21 20:59:18 +0200

    rust/sdl2sketch: added color-breeding to gameoflife example

--- examples/gameoflife.rs
+++ examples/gameoflife.rs
@@ -11,6 +11,7 @@ const HEIGHT: i32 = WIDTH;
 const BS: i32 = 2;
 const NROWS: i32 = (WIDTH+1) / BS;
 const NCOLS: i32 = (HEIGHT+1) / BS;
+const INIT_LIFE_PROB: f32 = 0.06;
 
 type Cells = Vec<Cell>;
 
@@ -26,11 +27,11 @@ fn main() {
 }
 
 fn generate_random_start() -> Cells {
-	let mut rng = rand::thread_rng();
 	let mut universe: Cells = Vec::new();
 	for i in 0..NROWS {
 		for j in 0..NCOLS {
-			universe.push(Cell::new(i, j, rng.gen()));
+			let r: f32 = rand::thread_rng().gen();
+			universe.push(Cell::new(i, j, r < INIT_LIFE_PROB));
 		}
 	}
 	universe
@@ -84,21 +85,44 @@ impl Cell {
 	}
 
 	pub fn update(&mut self, universe: &Cells) {
+		let prev_alive = self.alive;
 		self.alive = match (self.alive, self.count_neighbours(universe)) {
 			(true, 2...3) => true,
-			(true, _) => false,
-			(false, 3) => true,
-			(false, _) => false,
+			(true, _)     => false,
+			(false, 3)    => true,
+			(false, _)    => false,
+		};
+
+		if !prev_alive && self.alive { // if reborn
+			self.color = self.get_avg_hue(universe);
 		}
 	}
 
 	fn count_neighbours(&self, universe: &Cells) -> i32 {
-		let neighbours = self.get_neighbour_indices(true);
+		let neighbour_indices = self.get_neighbour_indices(true);
 		let mut count = 0;
-		for i in neighbours {
+		for i in neighbour_indices {
 			if universe[i].alive { count += 1; }
 		}
 		count
+	}
+
+	fn get_avg_hue(&self, universe: &Cells) -> Color {
+		let neighbour_indices = self.get_neighbour_indices(true);
+		let mut sum = 0;
+		let mut count = 0;
+		for i in neighbour_indices {
+			if universe[i].alive {
+				let hsv = utils::rgb_to_hsv(universe[i].color.r, universe[i].color.g, universe[i].color.b);
+				sum += hsv.0;
+				count += 1;
+			}
+		}
+
+		let avg_hue = (sum as f32 / count as f32).round() as u16;
+		let rgb = utils::hsv_to_rgb(avg_hue, 1.0, 1.0);
+
+		Color::RGB(rgb.0, rgb.1, rgb.2)
 	}
 
 	fn get_neighbour_indices(&self, wrap: bool) -> Vec<usize> {
